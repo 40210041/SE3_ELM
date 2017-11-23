da1613f9aec16f9993314dae1aef3e54c49dc8c3
40210041 <40210041@live.napier.ac.uk>
2017-11-23 16:45:12 +0000

    test unit files
    the files for unit tests

---
+++ ELM_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ELM_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ELM_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3633cd5-3518-4a9b-8654-7e5b38cc09d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ELM_Test/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ELM_40210041;
+
+namespace ELM_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //check append lists is tweet
+        [TestMethod]
+        public void checkType_InputIsTweet_ReturnTrue()
+        {
+            // arrange
+            var mainWindow = new MainWindow();
+            string typeistweet = "Tweet";
+            // act
+            var result = mainWindow.checkType_Tweet(typeistweet);
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        //check append lists is not tweet
+        [TestMethod]
+        public void checkType_InputIsNotTweet_ReturnFalse()
+        {
+            // arrange
+            var mainWindow = new MainWindow();
+            string typeistweet = "Tweeet";
+            // act
+            var result = mainWindow.checkType_Tweet(typeistweet);
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        //checks body type is sms
+        [TestMethod]
+        public void check_BodyType_IsSMS_IsEqual()
+        {
+            // arrange
+            var mainWindow = new MainWindow();
+            string bodyContent_SMS = "SMS";
+            int generatedID_SMS = 123456789;
+            // act
+            var result = mainWindow.check_BodyType(bodyContent_SMS, generatedID_SMS);
+            // assert
+            Assert.AreEqual(2,result);
+        }
+
+        //check body type is tweet
+        [TestMethod]
+        public void check_BodyType_IsTweet_IsEqual()
+        {
+            // arrange
+            var mainWindow = new MainWindow();
+            string bodyContent_Tweet = "Tweet";
+            int generatedID_Tweet = 123456789;
+            // act
+            var result = mainWindow.check_BodyType(bodyContent_Tweet, generatedID_Tweet);
+            // assert
+            Assert.AreEqual(1, result);
+        }
+
+        //check body type email
+        [TestMethod]
+        public void check_BodyType_IsEMail_IsEqual()
+        {
+            // arrange
+            var mainWindow = new MainWindow();
+            string bodyContent_EMail = "E-Mail";
+            int generatedID_EMail = 123456789;
+            // act
+            var result = mainWindow.check_BodyType(bodyContent_EMail, generatedID_EMail);
+            // assert
+            Assert.AreEqual(3, result);
+        }
+    }
+}
